84db4acc28fa8632380313ca73f02c129e6fd0c9
Peter Onneby <peter@onneby.com>
2021-08-09 00:05:14 +0200

    Add some more players

--- src/routes/players.js
+++ src/routes/players.js
@@ -128,18 +128,8 @@ router.get("/:id", authenticateJWT, async function (req, res) {
  *
  */
 router.post("/", authenticateJWT, async function (req, res) {
-  const { name, nationality, dateOfBirth, preferredFoot } = req.body;
-
-  let player = {
-    name,
-    nationality,
-    dateOfBirth,
-    preferredFoot,
-  };
-
-  const result = await playerService.createPlayer(player);
-
-  res.status(200).json(result);
+  const player = await playerService.createPlayer(req.body);
+  res.status(200).json(player);
 });
 
 /**
